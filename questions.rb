6497786a6efe04e3aa0cdb973906dabfb23b4fee
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-01-17 10:53:07 +0000

    Fixed 'select emlements starting with vowel'

--- questions.rb
+++ questions.rb
@@ -5,7 +5,7 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-
+  array.grep(/^[aeiou]/i)
 end
 
 # remove instances of nil (but NOT false) from an array
